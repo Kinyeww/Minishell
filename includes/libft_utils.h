6436f032b848ade149ff6c3f250e8aaea549d6f3
Kinyeww <cheekinyew123@gmail.com>
2026-05-18 18:48:01 +0800

    norm

--- includes/libft_utils.h
+++ includes/libft_utils.h
@@ -8,7 +8,7 @@ int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strlen(char *str);
 char	*ft_strdup(char *string);
 char	*ft_strchr(char *string, char c);
-char 	*ft_strndup(char *string , int size);
+char	*ft_strndup(char *string , int size);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 int		ft_isalnum(int c);
