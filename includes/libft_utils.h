cbb8dc0320af51c99282c5ed2f710eb845874328
Kinyeww <cheekinyew123@gmail.com>
2026-05-18 18:45:36 +0800

    added header for libftutil to silent warning

--- includes/libft_utils.h
+++ includes/libft_utils.h
@@ -2,15 +2,15 @@
 # define LIBFT_UTILS_H
 
 // #include "../../includes/minishell.h"
-int	ft_strcmp(const char *s1, const char *s2);
-int ft_strlen(char *str);
-char *ft_strdup(char *string);
-char *ft_strchr(char *string, char c);
-char *ft_strndup(char *string , int size);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strlen(char *str);
+char	*ft_strdup(char *string);
+char	*ft_strchr(char *string, char c);
+char 	*ft_strndup(char *string , int size);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
